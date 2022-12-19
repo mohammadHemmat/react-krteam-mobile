d1641340b89bb4285a755c1c1dd8bd594da5ceaf
jmowlaei <50267396+jmowlaei@users.noreply.github.com>
2022-12-19 19:47:25 +0330

    x

--- src/pages/traffic.jsx
+++ src/pages/traffic.jsx
@@ -46,6 +46,21 @@ function Traffic() {
     });
     map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
     if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            console.log(result.state);
+            //If granted then you can directly call your function here
+          } else if (result.state === "prompt") {
+            console.log(result.state);
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+          result.onchange = function () {
+            console.log(result.state);
+          };
+        });
       navigator.geolocation.watchPosition(function (position) {
         map.current.flyTo({
           center: [position.coords.longitude, position.coords.latitude]
